cfef50cd6c9c2fe645622c000b8d1d9cd46c2b27
Assolita <118940874+Assolita@users.noreply.github.com>
2025-11-18 18:50:09 -0300

    1

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -33,12 +33,13 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
+  port: 6543, // <- PORTA DO PGBOUNCER DO SUPABASE
   dialect: 'postgres',
   dialectOptions: {
     ssl: { require: true, rejectUnauthorized: false }
   },
   pool: {
-    max: 5,   // limite menor que o permitido pelo Supabase
+    max: 1,   // PGBOUNCER aceita uma conexão por instância
     min: 0,
     acquire: 30000,
     idle: 10000
