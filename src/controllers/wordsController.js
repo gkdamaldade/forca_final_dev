97d8d3ffef427d9118f09d59a89a0b8aaab0751d
gkdamaldade <133769577+gkdamaldade@users.noreply.github.com>
2025-11-19 00:47:47 -0300

    Update wordsController.js

--- src/controllers/wordsController.js
+++ src/controllers/wordsController.js
@@ -1,18 +1,12 @@
+const { getRandomWord } = require('./services/wordService.js');
 
-const { getRandomWord } = require('../services/wordService');
+async function iniciarNovoJogo(categoriaEscolhida) {
+    const palavraDB = await getRandomWord({ categoria: categoriaEscolhida });
 
-async function getRandom(req, res, next) {
-  try {
-    const { categoryId, category } = req.query;
-    const word = await getRandomWord({ categoryId, category, onlyActive: true });
-    if (!word) return res.status(404).json({ message: 'Nenhuma palavra encontrada.' });
-    res.json({
-      id: word.id,
-      text: word.text,
-      hint: word.hint,
-      category: word.category ? { id: word.category.id, name: word.category.name, slug: word.category.slug } : null
-    });
-  } catch (err) { next(err); }
+    const palavra = palavraDB.palavra;
+    const categoria = palavraDB.categoria;
+
+    jogoAtual = new Game(palavra, categoria);
+
+    return jogoAtual.getEstado();
 }
-
-module.exports = { getRandom };
