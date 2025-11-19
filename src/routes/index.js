573bfa4aa18abdea97266bba27392cf1399ec4ce
gkdamaldade <133769577+gkdamaldade@users.noreply.github.com>
2025-11-18 23:56:47 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -50,66 +50,7 @@ router.post('/usar-poder', (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
-
-router.post('/cadastro', async (req, res) => {
-    try {
-        // req.body vai conter { username, email, password }
-        const novoJogador = await lidarCadastro(req.body);
-        
-        // 201 Created - O padrão para criação bem-sucedida
-        res.status(201).json(novoJogador); 
-
-    } catch (error) {
-        console.error("Erro em POST /cadastro:", error.message);
-        
-        // Checa se é um erro de "usuário já existe"
-        if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(400).json({ message: "Email ou usuário já cadastrado." });
-        }
-
-        res.status(500).json({ message: "Erro interno ao cadastrar." });
-    }
-});
-
-
-router.post('/login', async (req, res) => {
-    try {
-        // req.body vai conter { email, password }
-        const loginData = await lidarLogin(req.body);
-        
-        // Retorna 200 OK com o token
-        res.status(200).json(loginData); 
-
-    } catch (error) {
-        console.error("Erro em POST /login:", error.message);
-        
-        // Se for "Credenciais inválidas", retorna 401 (Não Autorizado)
-        if (error.message === "Credenciais inválidas.") {
-            return res.status(401).json({ message: error.message });
-        }
-
-        res.status(500).json({ message: "Erro interno ao fazer login." });
-    }
-});
-
-router.post('/registrar-vitoria', async (req, res) => {
-    try {
-        const resultado = await registrarVitoria(req.body);
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.error("Erro ao registrar vitória:", error.message);
-        res.status(500).json({ message: "Erro ao salvar vitória." });
-    }
-});
-
-router.get('/ranking', async (req, res) => {
-    try {
-        const ranking = await listarRanking();
-        res.status(200).json(ranking);
-    } catch (error) {
-        // ...
-    }
-});
+// Retirado funções
 
 router.post('/tempo-esgotado', (req, res) => {
     try {
