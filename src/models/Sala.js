ad8ebe9608a672552d2fc906e7297026d0695963
gkdamaldade <133769577+gkdamaldade@users.noreply.github.com>
2025-11-19 00:04:48 -0300

    Update Sala.js

--- src/models/Sala.js
+++ src/models/Sala.js
@@ -18,7 +18,6 @@ class Sala extends Model {
   }
 
   static associate(models) {
-    this.hasMany(models.Player, { foreignKey: 'sala_id', as: 'players' });
     this.hasMany(models.Result, { foreignKey: 'sala_id', as: 'results' });
     this.hasMany(models.GameM, { foreignKey: 'sala_id', as: 'games' }); // se quiser vincular jogos à sala
   }
